6690ee81a2601594ffff801471ba34f2ffe4071b
zouyalong <zouyalong@bytedance.com>
2023-02-26 14:26:20 +0800

    使用 cargo build --target x86_64-unknown-none 编译成功

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // std 由OS提供
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
